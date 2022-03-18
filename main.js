5a1871da399773540389a1de3af70c2528c3379e
KonaDaniel <79358753+KonaDaniel@users.noreply.github.com>
2022-03-18 16:33:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function setup() {
+    canvas = createCanvas(500, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(500, 500);
+    video.position(1190, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotResults);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is ready for duty!');
+}
+
+function gotResults(results) {
+    if(results.lenght > 0) {
+        console.log(results);
+    }
+}
+
+function draw() {
+    background('white');
+}
